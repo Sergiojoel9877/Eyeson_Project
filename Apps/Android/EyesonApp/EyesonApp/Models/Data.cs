ed499088ed7bfa61aaf6e109633643db37fe05e3
Sergio Joel Ferreras <sergiojoel9877@hotmail.com>
2019-06-09 23:00:13 -0400

    feat: Add Data model, to represent the data incoming over TCP/Socket connections

---
+++ Apps/Android/EyesonApp/EyesonApp/Models/Data.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json;
+
+namespace EyesonApp.Models
+{
+    public class Data
+    {
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [JsonProperty("password")]
+        public string Password { get; set; }
+
+        [JsonProperty("ip")]
+        public string Ip { get; set; }
+
+        [JsonProperty("port")]
+        public long Port { get; set; }
+
+        [JsonProperty("camera")]
+        public long Camera { get; set; }
+
+        [JsonProperty("date")]
+        public DateTimeOffset Date { get; set; }
+    }
+}
