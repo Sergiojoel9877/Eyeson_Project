cb9912f09bec79cea0e645120334736d8e30488f
Sergio Joel Ferreras <sergiojoel9877@hotmail.com>
2019-06-09 23:02:35 -0400

    feat: Add a Singleton Data class to manage the global instance of the data pulled from the TCP/Socket.

---
+++ Apps/Android/EyesonApp/EyesonApp/Services/DataSingleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using EyesonApp.Models;
+
+namespace EyesonApp.Services
+{
+    public class DataSingleton
+    {
+        static Data _Data { get; set; }
+  
+        public static Data Instance()
+        {
+            if (_Data == null)
+            {
+                _Data = new Data();
+            }
+            return _Data;
+        } 
+
+        public static Data SetInstance(Data _data) => _Data = _data;
+
+        public static void SetInstanceToNull()
+        {
+            _Data = null;
+        }
+    }
+}
