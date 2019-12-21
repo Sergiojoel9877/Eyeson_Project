09c4194a30de4bfcea7a328e32275f74ac3a8132
Sergio Joel Ferreras Batista <sergiojoel9877@gmail.com>
2019-12-21 00:34:24 -0400

    Feat: Add helper singletons to manage the global state of the app.

--- Apps/Android/EyesonApp/EyesonApp/Services/SaveLoginSessionSingleton.cs
+++ Apps/Android/EyesonApp/EyesonApp/Services/SaveLoginSessionSingleton.cs
@@ -12,6 +12,7 @@ using Android.Widget;
 
 namespace EyesonApp.Services
 {
+    [Preserve(AllMembers = true)]
     public class SaveLoginSessionSingleton
     {
         static bool IsLoggedIn { get; set; } = false;
@@ -26,5 +27,68 @@ namespace EyesonApp.Services
         }
 
         public static bool SaveUserLoggedSession(bool save) => IsLoggedIn = save;
+    }
+
+    [Preserve(AllMembers = true)]
+    public class SaveOnResumeStateSingleton
+    {
+        static bool OnResume { get; set; } = false;
+
+        public static bool ResumeStarted()
+        {
+            if (OnResume)
+            {
+                OnResume = true;
+            }
+            return OnResume;
+        }
+
+        public static bool SaveOnResumeState(bool save) => OnResume = save;
+    }
+
+    [Preserve(AllMembers = true)]
+    public class OnPauseStateSingleton
+    {
+        static bool OnPause { get; set; } = false;
+
+        static int Progress { get; set; } = -1;
+
+        public static bool PauseStarted()
+        {
+            if (OnPause)
+            {
+                OnPause = true;
+            }
+            return OnPause;
+        }
+
+        public static int ProgressValue()
+        {
+            if (Progress >= -1)
+            {
+                return Progress;
+            }
+            return -1;
+        }
+
+        public static bool SaveOnPauseState(bool save) => OnPause = save;
+
+        public static int SaveProgressState(int progress) => Progress = progress;
+    }
+
+    public class OnResumePreviewStateSingleton
+    {
+        static bool OnResume { get; set; } = false;
+
+        public static bool ResumeStarted()
+        {
+            if (OnResume)
+            {
+                return OnResume = true;
+            }
+            return OnResume;
+        }
+
+        public static bool SetState(bool state) => OnResume = state;
     }
 }
